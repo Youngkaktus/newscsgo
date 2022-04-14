66634c0151f36db1c714e704e27efef71ab09666
Youngkaktus <103257884+Youngkaktus@users.noreply.github.com>
2022-04-14 17:52:03 +0300

    Delete newscsgo directory

--- newscsgo/app/include/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$link = mysqli_connect('localhost', 'root', '', 'articles');
-
-if(mysqli_connect_errno())
-{
-  echo 'Ошбка в подключении к базе данных ('.mysqli_connect_errno().'): '.mysqli_connect_error();
-  exit();
-}
